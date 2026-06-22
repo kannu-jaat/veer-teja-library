e27ed88bd9177f92bed940be2d8009615884c861
kannu-jaat <krix7096@gmail.com>
2026-06-22 19:06:24 +0530

    Update AppConfig.java

--- app/src/main/java/com/google/android/youtube/pro/AppConfig.java
+++ app/src/main/java/com/google/android/youtube/pro/AppConfig.java
@@ -1,10 +1,8 @@
 package com.google.android.youtube.pro;
 
 public class AppConfig {
-    // Kisi aur library ko sell karte waqt bas yahan naam badal dena
-    public static final String LIBRARY_NAME = "KRISHNA LIBRARY";
-    
-    // Future ke liye (jaise Admin ka UPI ID, etc.) hum yahan add karte rahenge
-    public static final String CONTACT_EMAIL = "admin@krishnalibrary.com";
+    public static final String LIBRARY_NAME = "Krishna Library 1";
+    public static final String TAGLINE = "Smart Attendance • Easy Learning";
+    public static final String HELP_EMAIL = "support@krishnalibrary.com";
+    public static final String CONTACT_NUMBER = "+91 9876543210";
 }
-
