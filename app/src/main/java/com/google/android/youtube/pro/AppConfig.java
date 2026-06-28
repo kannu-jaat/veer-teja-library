5cbdb564cd9edf6f644b28c84eeea3443e85fa6e
kannu-jaat <krix7096@gmail.com>
2026-06-28 21:24:38 +0530

    Update AppConfig.java

--- app/src/main/java/com/google/android/youtube/pro/AppConfig.java
+++ app/src/main/java/com/google/android/youtube/pro/AppConfig.java
@@ -1,8 +1,14 @@
 package com.google.android.youtube.pro;
 
 public class AppConfig {
-    public static final String LIBRARY_NAME = "Krishna Library 1";
-    public static final String TAGLINE = "Smart Attendance • Easy Learning 1";
+    // Basic Library Details
+    public static final String LIBRARY_NAME = "Krishna Library";
+    public static final String TAGLINE = "Smart Attendance • Easy Learning";
     public static final String HELP_EMAIL = "support@krishnalibrary.com";
     public static final String CONTACT_NUMBER = "+91 9876543210";
+
+    // Cloudinary Storage Details (Auto Folder System)
+    public static final String CLOUDINARY_CLOUD_NAME = "js1bambx";
+    public static final String CLOUDINARY_UPLOAD_PRESET = "attendance";
+    public static final String CLOUDINARY_API_KEY = "339684396318929";
 }
