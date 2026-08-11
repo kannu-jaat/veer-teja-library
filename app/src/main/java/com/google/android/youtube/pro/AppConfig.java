d688c2b884e8a3dbb02210d8914f83990bb0a33a
kannu-jaat <krix7096@gmail.com>
2026-08-11 19:42:28 +0530

    lib. name test AppConfig.java

--- app/src/main/java/com/google/android/youtube/pro/AppConfig.java
+++ app/src/main/java/com/google/android/youtube/pro/AppConfig.java
@@ -2,7 +2,7 @@ package com.google.android.youtube.pro;
 
 public class AppConfig {
     // Basic Library Details
-    public static final String LIBRARY_NAME = "Krishna Library";
+    public static final String LIBRARY_NAME = "Krishna Library03";
     public static final String TAGLINE = "Smart Attendance • Easy Learning";
     public static final String HELP_EMAIL = "support@krishnalibrary.com";
     public static final String CONTACT_NUMBER = "+91 9876543210";
