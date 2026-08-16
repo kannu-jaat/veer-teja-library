a4d8f85206ec222e8394eea9b1aa7e0f84bdfde0
kannu-jaat <krix7096@gmail.com>
2026-08-16 23:03:24 +0530

    Create PortraitCaptureActivity.java

---
+++ app/src/main/java/com/google/android/youtube/pro/PortraitCaptureActivity.java
@@ -0,0 +1,7 @@
+package com.google.android.youtube.pro;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class PortraitCaptureActivity extends CaptureActivity {
+    // Khaali chhod do, bas class extend karni thi Portrait mode ke liye!
+}
